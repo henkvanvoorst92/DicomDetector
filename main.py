c9f937edf8d6303233cd677cbed30eafb1f5389a
henkvanvoorst92 <henkvanvoorst92@gmail.com>
2026-07-09 10:44:02 -0700

    upd

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from multiprocessing import Pool
+from utils import get_general_args
+from get_metadata import get_all_metadata, get_full_combined, chunk_jobs
+
+if __name__ == "__main__":
+    args = get_general_args()
+
+    print(f'--------- Processing {args.input} ---------')
+    if args.njobs==1:
+        get_all_metadata(args.input, args.output, file_limits=(5, 1e6), multipos_limits=(0, 1e6), skip_tags=['Privatetagdata'], export_to='.xlsx')
+    else:
+        jobs = chunk_jobs(args.input, args.output, jobs=args.njobs, file_limits=(5,1e6), multipos_limits=(0, 1e6), skip_tags=['Privatetagdata'], export_to='.xlsx')
+        with Pool(processes=args.njobs) as pool:
+            pool.starmap(get_all_metadata, jobs)
+
+    get_full_combined(args.output, redo_timestamps=True, redo_labelling=True)
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+conda activate base
+
+python main.py --input /pat/to/input/dir --output /path/to/output/dir --njobs 1
